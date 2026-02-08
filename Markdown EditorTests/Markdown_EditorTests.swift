4641153365f30f821f2cfcc5c9814cfa53110ffa
Allen Hammock <ahammock@mac.com>
2026-02-07 19:13:21 -0800

    Initial Commit

---
+++ Markdown EditorTests/Markdown_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Markdown_EditorTests.swift
+//  Markdown EditorTests
+//
+//  Created by ahammock on 2/7/26.
+//
+
+import Testing
+@testable import Markdown_Editor
+
+struct Markdown_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Markdown EditorUITests/Markdown_EditorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Markdown_EditorUITestsLaunchTests.swift
+//  Markdown EditorUITests
+//
+//  Created by ahammock on 2/7/26.
+//
+
+import XCTest
+
+final class Markdown_EditorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
